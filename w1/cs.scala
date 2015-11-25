94a5a6a7e108c76506f239660a4844089c89ad78
mkoltsov <root@javabean.ru>
2015-11-25 10:02:38 +0100

    call by name, call by value examples added

--- w1/cs.scala
+++ w1/cs.scala
@@ -1,5 +1,12 @@
+//call by value + call by name
 def and(x:Boolean, y: => Boolean) = if (x) y else false
 
 println(and(true, false))
 println(and(false, true))
 println(and(true, true))
+
+def loop:Boolean = loop
+// call by name
+def a = loop
+// call by value
+//val b = loop
