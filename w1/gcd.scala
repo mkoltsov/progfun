5d8f15326befcd7729e521f429cb4b1331a22c03
mkoltsov <root@javabean.ru>
2015-11-26 13:56:24 +0100

    factorial example added

--- w1/gcd.scala
+++ w1/gcd.scala
@@ -1,4 +1,9 @@
+@tailrec
 def gcd(a:Int, b:Int):Int = if (b==0) a else gcd(b, a%b)
 
 println(gcd(100,20))
 println(gcd(111,27))
+
+def factorial(a:Int):Int = if (a==0) 1 else a* factorial(a-1)
+
+println(factorial(4))
