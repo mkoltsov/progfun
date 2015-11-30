3c84825efe6e201ad870b43bb336584e6023c4da
mkoltsov <root@javabean.ru>
2015-11-30 23:25:07 +0100

    currying is equal when you break a function with multiple paramaters into a number of functions that take some of these parameters

--- w2/currying.scala
+++ w2/currying.scala
@@ -24,3 +24,18 @@ println(sd(1,10))
 def sum3(f:Int => Int) (a:Int*) = println(f(a(3)))
 
 sum3(x=>x)(1,3,4,5,6)
+
+def product(f:Int => Int) (a:Int, b:Int):Int=
+	if (a>b) 1
+	else f(a) * product(f)(a+1,b)
+println(product(x=>x*x)(3,4))
+def fact(n: Int) = product(x=>x)(1,n)
+println(fact(5))
+
+def mapReduce(f:Int=>Int, combine:(Int, Int) => Int, zero:Int) (a: Int, b:Int):Int =
+	if (a>b) zero
+	else combine(f(a), mapReduce(f, combine, zero)(a+1, b))
+
+def product2(f:Int => Int) (a:Int, b:Int):Int= mapReduce(f, (x,y) => x*y, 1)(a, b)
+
+println(product2(x=>x*x)(3,4))
