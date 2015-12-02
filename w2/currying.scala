02aa12897c2aec6356b84fa6d9d79df6827eaa0a
mkoltsov <root@javabean.ru>
2015-12-02 22:54:59 +0100

    examples of currying added

---
+++ w2/currying.scala
@@ -0,0 +1,22 @@
+def sum(f:Int => Int):(Int, Int) => Int = {
+	def sumF(a: Int, b:Int):Int = {
+		if (a>b) 0
+		else f(a) + sumF(a+1, b)
+
+	}
+	sumF
+}
+
+def sumInts = sum(x=>x)
+def sumCubes = sum(x=>x*x*x)
+
+println(sumCubes(1,10))
+println(sum(x=>x*x*x)(1,10))
+
+def sum2(f:Int => Int) (a:Int, b:Int):Int = if (a>b) 0 else f(a) + sum(f)(a+1, b)
+
+println(sum2(x=>x*x*x)(1,10))
+
+val sd = sum2(x=>x*x*x)_
+
+println(sd(1,10))
