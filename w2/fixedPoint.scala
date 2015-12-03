e78a3ff1f06a05f3420b79f46164369c09ef6996
mkoltsov <root@javabean.ru>
2015-12-03 23:51:05 +0100

    more examples of hof added

--- w2/fixedPoint.scala
+++ w2/fixedPoint.scala
@@ -19,5 +19,10 @@ println(fixedPoint(x => 1+x/2)(1))
 
 def sqrt(x:Double) = fixedPoint(y => (y+x/y)/2)(1.0)
 
+def averageDamp(f:Double => Double)(x:Double) = (x+ f(x))/2
+
 println(sqrt(81))
 
+def sqrt2(x:Double) = fixedPoint(averageDamp(y => x/y))(1)
+
+println(sqrt2(81))
