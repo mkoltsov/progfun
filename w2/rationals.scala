dd92f685f5e18329e5808e82cf211f973c8fe514
mkoltsov <root@javabean.ru>
2015-12-04 15:55:01 +0100

    rational numbers examples added

--- w2/rationals.scala
+++ w2/rationals.scala
@@ -1,8 +1,14 @@
 class Rational(x:Int, y:Int) {
-	def numer = x
-	def denom = y
+	require(y != 0, "denominator must be non zero")
+	private def gcd(a:Int, b:Int):Int = if (b==0) a else gcd(b, a % b)
+	private val g = gcd(x, y)
+	def numer = x/g
+	def denom = y/g
 	def add(that:Rational) = new Rational(numer * that.denom+that.numer * denom, denom * that.denom)
-	def sub(that:Rational) = new Rational(numer * that.denom-that.numer * denom, denom * that.denom)
+	def sub(that:Rational) = add(that.neg)
+
+	def less (that:Rational) = numer*that.denom < that.numer * denom
+	def max (that:Rational) = if (this.less(that)) that else that
 
 	override def toString() = s"${numer}/${denom}"
 
@@ -18,3 +24,7 @@ println(rat.neg)
 println(rat.add(rat2))
 
 println(new Rational(1,3).sub(new Rational(5,7)).add(new Rational(3,2).neg))
+
+println(rat.less(rat2))
+println(rat.max(rat2))
+val rat3 = new Rational(1,0)
