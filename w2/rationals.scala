a7860b1845d2113d370da0bbc82c31204223c374
mkoltsov <root@javabean.ru>
2015-12-06 23:34:30 +0100

    more rationals added

--- w2/rationals.scala
+++ w2/rationals.scala
@@ -2,15 +2,17 @@ class Rational(x:Int, y:Int) {
 	require(y != 0, "denominator must be non zero")
 	private def gcd(a:Int, b:Int):Int = if (b==0) a else gcd(b, a % b)
 	private val g = gcd(x, y)
-	def numer = x/g
-	def denom = y/g
+
+	def this(x:Int) = this(x,1)
+	def numer = x
+	def denom = y
 	def add(that:Rational) = new Rational(numer * that.denom+that.numer * denom, denom * that.denom)
 	def sub(that:Rational) = add(that.neg)
 
 	def less (that:Rational) = numer*that.denom < that.numer * denom
 	def max (that:Rational) = if (this.less(that)) that else that
 
-	override def toString() = s"${numer}/${denom}"
+	override def toString() = s"${numer/g}/${denom/g}"
 
 	def neg() = new Rational(-numer, denom) 
 }
@@ -25,6 +27,7 @@ println(rat.add(rat2))
 
 println(new Rational(1,3).sub(new Rational(5,7)).add(new Rational(3,2).neg))
 
+val f = new Rational(1)
 println(rat.less(rat2))
 println(rat.max(rat2))
-val rat3 = new Rational(1,0)
+// val rat3 = new Rational(1,0)
