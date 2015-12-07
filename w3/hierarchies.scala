b172d1100a9dcfefe0f24c6596776e11b76d5859
mkoltsov <root@javabean.ru>
2015-12-07 14:32:12 +0100

    examples of class hierarchies added

--- w3/hierarchies.scala
+++ w3/hierarchies.scala
@@ -1,21 +1,25 @@
-abstract class IntSet {
-	def incl(x:Int): IntSet
-	def contains(x:Int): Boolean
-}
+// abstract class IntSet {
+// 	def incl(x:Int): IntSet
+// 	def contains(x:Int): Boolean
+// }
 
-class NonEmpty(elem:Int, left:IntSet, right:IntSet) extends IntSet {
-	def contains(x:Int):Boolean = 
-		if (x < elem) left contains x
-		else if (x> elem) right contains x
-		else true
-	def incl(x: Int):IntSet = this/*
-		if (x < elem) new NonEmpty(elem, left incl x, right)
-		if (x > elem) new NonEmpty(elem, left, right incl x)
-		else this*/
-}
+// class NonEmpty(elem:Int, left:IntSet, right:IntSet) extends IntSet {
+// 	def contains(x:Int):Boolean = 
+// 		if (x < elem) left contains x
+// 		else if (x> elem) right contains x
+// 		else true
+// 	def incl(x: Int):IntSet = this/*
+// 		if (x < elem) new NonEmpty(elem, left incl x, right)
+// 		if (x > elem) new NonEmpty(elem, left, right incl x)
+// 		else this*/
+// }
 
-object Empty extends IntSet {
-	def contains(x: Int) = false
-	def incl(x: Int) = new NonEmpty(x, Empty, Empty)
-}
+// object Empty extends IntSet {
+// 	def contains(x: Int) = false
+// 	def incl(x: Int) = new NonEmpty(x, Empty, Empty)
+// }
 
+object Hello {
+	def main(args: Array[String]) = println("Hello")
+	
+}
