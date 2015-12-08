4cfd201e0edd28e820fe44c30e32112e2922522c
mkoltsov <root@javabean.ru>
2015-12-08 01:08:41 +0100

    examples of functions as objects added

--- w4/func.scala
+++ w4/func.scala
@@ -12,7 +12,7 @@ object List {
 	def apply[T](x:T, y:T):AnyRef = new AnyRef
 	
 }
-
+//generics can be omited, since compiler can guess the type by itself
 println(List[Int](1))
 println(List(1))
 println(List())
