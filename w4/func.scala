f4656ad4fcd2495a17dd5695803ff83a79e21843
mkoltsov <root@javabean.ru>
2015-12-09 01:07:29 +0100

    examples of functions as objects added

---
+++ w4/func.scala
@@ -0,0 +1,19 @@
+//eta expansion
+val tripleArg = (x:Int) => x* doubleArg(x)
+
+println(tripleArg(7))
+
+def doubleArg(x:Int):Int = x*x
+
+// List(1) = List.apply(1)
+object List {
+	def apply[T](x:T):AnyRef = new AnyRef
+	def apply[T]():AnyRef = new AnyRef
+	def apply[T](x:T, y:T):AnyRef = new AnyRef
+	
+}
+
+println(List[Int](1))
+println(List(1))
+println(List())
+println(List(1,2))
