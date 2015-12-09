9385f1c99f154d301a593cf43a61096467b212c3
mkoltsov <root@javabean.ru>
2015-12-09 12:37:37 +0100

    using subtyping for oo-approach, generics for fp-approach

--- w4/subtypingGenerics.scala
+++ w4/subtypingGenerics.scala
@@ -1,4 +1,7 @@
-def assertAllPos[S <: AnyVal](r:S) = if (r != null) r else throw new IllegalArgumentException
+// S <: T means :S is a subtype of T
+// S >: T means :S is a supertype of T, or T is a subtype of S
+def assertAllPos[S >: AnyVal <: Any](r:S):S = if (r != null) r else throw new IllegalArgumentException
 
 println(assertAllPos(13))
 println(assertAllPos(2.0))
+println(assertAllPos(true))
