938f890eb0474f1b30b8f47df5f902cd3fec43bb
mkoltsov <root@javabean.ru>
2015-12-11 14:30:20 +0100

    examples of type casts added

--- w4/decomposition.scala
+++ w4/decomposition.scala
@@ -1,4 +1,5 @@
 trait Expr {
+	def eval:Int
 	def isNumber:Boolean
 	def isSum:Boolean
 	def numValue:Int
@@ -7,6 +8,7 @@ trait Expr {
 }
 
 class Number(n:Int) extends Expr {
+	def eval = n
 	def isNumber:Boolean = true
 	def isSum:Boolean = false
 	def numValue:Int = n 
@@ -15,6 +17,7 @@ class Number(n:Int) extends Expr {
 }
 
 class Sum(e1:Expr, e2:Expr) extends Expr {
+	def eval = e1.eval + e2.eval
 	def isNumber = false
 	def isSum = true
 	def numValue = throw new Error("Sum.numValue")
