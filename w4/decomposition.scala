ead991a78d3b4375982dcf071e337caf5108ce41
mkoltsov <root@javabean.ru>
2015-12-11 14:23:07 +0100

    examples of type casts added

---
+++ w4/decomposition.scala
@@ -0,0 +1,35 @@
+trait Expr {
+	def isNumber:Boolean
+	def isSum:Boolean
+	def numValue:Int
+	def leftOp:Expr
+	def rightOp:Expr
+}
+
+class Number(n:Int) extends Expr {
+	def isNumber:Boolean = true
+	def isSum:Boolean = false
+	def numValue:Int = n 
+	def leftOp:Expr = throw new Error("Number.leftOp")
+	def rightOp:Expr = throw new Error("Number.rightOp")
+}
+
+class Sum(e1:Expr, e2:Expr) extends Expr {
+	def isNumber = false
+	def isSum = true
+	def numValue = throw new Error("Sum.numValue")
+	def leftOp = e1
+	def rightOp = e2
+}
+
+val num1 = new Number(10)
+val num2 = new Sum(new Number(5), num1)
+
+println(s"is Number ${num1.isInstanceOf[Number]}")
+// println(s"is Nothing ${num1.isInstanceOf[Nothing]}")
+println(s"is Any${num1.isInstanceOf[Any]}")
+println(s"is AnyRef ${num1.isInstanceOf[AnyRef]}")
+println(s"can be AnyRef ${num1.asInstanceOf[AnyRef].isInstanceOf[AnyRef]}")
+println(s"can be Any ${num1.asInstanceOf[AnyRef].isInstanceOf[Any]}")
+println(s"can be Number ${num1.asInstanceOf[AnyRef].isInstanceOf[Number]}")
+// println(s"can be Nothing ${num1.asInstanceOf[AnyRef].isInstanceOf[Nothing]}")
