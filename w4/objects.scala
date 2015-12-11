fa69c5e07fdeee1dbd257f6769e739e1700577e3
mkoltsov <root@javabean.ru>
2015-12-11 11:29:51 +0100

    peano numbers added

--- w4/objects.scala
+++ w4/objects.scala
@@ -7,3 +7,26 @@ object true extends Boolean {
 object false extends Boolean {
 	def ifThenElse[T](t: => T, e: => T) = e
 }
+//Peano numbers
+abstract class Nat {
+	def isZero:Boolean
+	def predecessor:Nat
+	def successor:Nat = new Succ(this)
+	def + (that:Nat):Nat
+	def - (that:Nat):Nat
+}
+
+object Zero extends Nat {
+	def isZero:Boolean = true
+	def predecessor:Nat = throw new Error("0.predecessor")
+	def + (that:Nat):Nat = that
+	def - (that:Nat):Nat = if (that.isZero) this else throw new Error("negative number")
+}
+
+
+object Succ(n:Nat) extends Nat {
+	def isZero:Boolean = false
+	def predecessor:Nat = n
+	def + (that:Nat):Nat = new Succ(n + that)
+	def - (that:Nat):Nat = if (that.isZero) this else n - that.predecessor
+}
