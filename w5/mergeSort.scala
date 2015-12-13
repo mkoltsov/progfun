a4dcac5ccdebed807e1a6e7316e62ca51bd55f71
mkoltsov <root@javabean.ru>
2015-12-13 22:03:59 +0100

    examples of using tuples/pairs added

---
+++ w5/mergeSort.scala
@@ -0,0 +1,24 @@
+val pair = ("chef", "pupa")
+val (label, value) = pair
+println(s"${label} = ${value}")
+println(s"${pair._1} = ${pair._2}")
+
+def msort(xs:List[Int]):List[Int] = {
+	val n = xs.length/2
+	if (n==0) xs 
+	else {
+		def merge(xs:List[Int], ys:List[Int]):List[Int] = (xs, ys) match {
+			case (Nil, ys) => ys
+			case (xs, Nil) => xs
+			case (x::xs1, y::ys1) => 
+				if (x<y) x :: merge(xs1, ys) 
+	 		   else y :: merge(xs, ys1)  
+		}
+		
+		val (fst, snd) = xs splitAt n 
+		merge(msort(fst), msort(snd))
+	}
+}
+
+val nums = List(2,-4,5,7,1)
+println(msort(nums))
