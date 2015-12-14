46bd177f5192a50b130c498dfee8050dfa2b431f
mkoltsov <root@javabean.ru>
2015-12-14 11:45:14 +0100

    examples of implicit parameters added

--- w5/mergeSort.scala
+++ w5/mergeSort.scala
@@ -3,22 +3,24 @@ val (label, value) = pair
 println(s"${label} = ${value}")
 println(s"${pair._1} = ${pair._2}")
 
-def msort(xs:List[Int]):List[Int] = {
+def msort[T](xs:List[T])(lt:(T, T) => Boolean):List[T] = {
 	val n = xs.length/2
 	if (n==0) xs 
 	else {
-		def merge(xs:List[Int], ys:List[Int]):List[Int] = (xs, ys) match {
+		def merge(xs:List[T], ys:List[T]):List[T] = (xs, ys) match {
 			case (Nil, ys) => ys
 			case (xs, Nil) => xs
 			case (x::xs1, y::ys1) => 
-				if (x<y) x :: merge(xs1, ys) 
+				if (lt(x,y)) x :: merge(xs1, ys) 
 	 		   else y :: merge(xs, ys1)  
 		}
 		
 		val (fst, snd) = xs splitAt n 
-		merge(msort(fst), msort(snd))
+		merge(msort(fst)(lt), msort(snd)(lt))
 	}
 }
 
 val nums = List(2,-4,5,7,1)
-println(msort(nums))
+println(msort(nums)((x:Int, y:Int)=> x<y))
+val fruits = List("apples", "oranges", "grapes", "peers")
+println(msort(fruits)((x, y)=> x.compareTo(y)< 0))
