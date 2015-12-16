ba0b3d2564b185be4d61708a39c93f1a7200e9e1
mkoltsov <root@javabean.ru>
2015-12-16 10:01:30 +0100

    collections exercises completed

--- w6/collections.scala
+++ w6/collections.scala
@@ -27,6 +27,14 @@ println(Vector(1,2,3,4).product)
 println(Vector(1,2,3,4).max)
 println(Vector(1,2,3,4).min)
 
-(1 to 1000) flatMap (x:Int=> (1 to 100) map (y=> (x,y)))
+println((1 to 1000).flatMap{x:Int => (x to 100).map(y => (x,y))})
 
 def scalarProduct(xs:Vector[Double], ys:Vector[Double]):Double = (xs zip ys).map(xy=> xy._1 * xy._2).sum
+//def scalarProduct2(xs:Vector[Double], ys:Vector[Double]):Double = (xs zip ys).map(case (x, y)=> x * y).sum
+
+println(scalarProduct(Vector(1,2,3,4), Vector(5,6,9,10)))
+//println(scalarProduct2(Vector(1,2,3,4), Vector(5,6,9,10)))
+
+def isPrime(n:Int):Boolean = !(2 until n).forall(x=> n%x!=0)
+
+println(isPrime(23))
